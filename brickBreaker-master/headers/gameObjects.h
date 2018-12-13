5ad4f4f674ede5c801bdf4fd81152c355be548c8
MasCair2k16 <gotgame6@msn.com>
2018-12-13 09:23:36 -0800

    Commented on function
    Function are now commented and positions are redefined to reassure there are not any replicated functions.

--- brickBreaker-master/headers/gameObjects.h
+++ brickBreaker-master/headers/gameObjects.h
@@ -1,3 +1,6 @@
+// Brick Breaker
+// by mason caird & benjamin greenwood
+
 #include <iostream>
 #include <string>
 #include <vector>
@@ -14,7 +17,7 @@ using namespace std;
 HANDLE hStdout, hStdin; 
 CONSOLE_SCREEN_BUFFER_INFO csbiInfo; 
 
-void ShowConsoleCursor(bool showFlag)
+void ShowConsoleCursor(bool showFlag)  // Microsoft standard function to run a terminal with active objects
 {
 	HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
     CONSOLE_CURSOR_INFO     cursorInfo;
@@ -23,13 +26,14 @@ void ShowConsoleCursor(bool showFlag)
     SetConsoleCursorInfo(out, &cursorInfo);
 }
 
+// Screen size
 const int MAX_X = 40;
 const int MAX_Y = 20;
 
-class GameObject {
+class GameObject { // This class will define the basic structures of each object.
     protected:
-        COORD pos;
-        double vy,vx;
+        COORD pos; // Postion of each gameobject child class
+        double vy,vx; // direction of each gameobject child class
     public:
         GameObject() { // Makes gameobject for the ball and still box.
 
@@ -39,7 +43,8 @@ class GameObject {
             vx = 0.0;
             vy = 0.0;
         }
-         virtual void draw() {	SetConsoleCursorPosition(hStdout,pos);}
+         virtual void draw() {	SetConsoleCursorPosition(hStdout,pos);} // This will print the characters to the screen.
+		// The function inside sets the character to microsoft standard
 
          COORD get_pos() {
             return pos;
@@ -48,7 +53,7 @@ class GameObject {
          virtual void move() {}
 };
 
-class stillBox : public GameObject {
+class stillBox : public GameObject {  // this is the stillbox class that defines all its properties.
     protected:
         string stillBoxShape;
     public:
@@ -61,15 +66,13 @@ class stillBox : public GameObject {
                 pos.X = rand() % MAX_X;
             }
             stillBoxShape = "[/////]";
-         // look at this (get it?? hehe)
-        
-
-            
-        }
-        short getX() { return pos.X; }
-        short getY() { return pos.X; }
-        void newX() {this->pos.X = rand() % MAX_X;}
-        void newY() {this->pos.Y = rand() & MAX_X;}
+  
+	
+	}
+        short getX() { return pos.X; } // Returning the position x
+        short getY() { return pos.Y; }  // Returning the position y
+        void newX() {this->pos.X = rand() % MAX_X;} // regenerating the x position
+        void newY() {this->pos.Y = rand() & MAX_X;} // regenerating the y position
         string getShape()
         {
             return stillBoxShape;
@@ -84,7 +87,7 @@ class stillBox : public GameObject {
         }
 };
 
-class userBox : public GameObject {
+class userBox : public GameObject {  // this is the userlbox class that defines all its properties.
     protected:
         string userBoxShape;
     public:
@@ -98,7 +101,7 @@ class userBox : public GameObject {
             GameObject::draw();
             cout << userBoxShape;
         }
-        void move(int key) {
+        void move(int key) { // Movement of the userBox
            /* if ( key != -2 ) {
                 cout << "You entered: " << key << endl;
                //exit(0);
@@ -117,11 +120,12 @@ class userBox : public GameObject {
         }
 };
 
-class Ball : public GameObject {
+class Ball : public GameObject { // this is the ball class that defines all its properties.
+    protected:
     protected:
         char ballShape_;
     public:
-        Ball(char ballShape) { 
+        Ball(char ballShape) { //takes in a character shape of a charcter
             ballShape_ = ballShape;
             pos.X = 20;
             pos.Y = 17;
@@ -133,12 +137,12 @@ class Ball : public GameObject {
             vy = vy*-1.0;
             this->pos.Y = 17;
         }
-        short getY() { return pos.Y; }
+        short getY() { return pos.Y; } // Returns position of y.
         void draw() {
             GameObject::draw();
             cout << ballShape_;
         }
-        void move() {
+        void move() { // Moves the ball with direction and position.
             pos.X += vx;
             pos.Y += vy;
             // KEEPS THE BALL INSIDE WINDOW EXCEPT THE BOTTOM
